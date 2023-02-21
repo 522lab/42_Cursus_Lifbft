4d6dedb38ac7413aa3a30a9fd41deffa50d7e8bf
522 <nakarinsupasatian@gmail.com>
2023-02-21 13:45:05 +0700

    add ft_isalnum.c

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ a project to create a library with a collection of functions
 ## ctype.h
 * [`ft_isalpha`](ft_isalpha.c)  : check alphabet
 * [`ft_isdigit`](ft_isdigit.c)  : check digit
+* [`ft_isalnum`](ft_isalnum.c)  : check alphanumeric
--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,22 +6,13 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 12:22:33 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/02/21 13:18:52 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/02/21 13:40:52 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 		return (1);
 	return (0);
 }
-/* 
-#include <stdio.h>
-int	main()
-{
-	printf("%d\n",ft_isalpha('1'));
-	printf("%d\n",ft_isalpha(' '));
-	printf("%d\n",ft_isalpha('a'));
-}
- */
