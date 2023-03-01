6facaab4942cffbefb5a3f8d8beb038b64021785
522 <nakarinsupasatian@gmail.com>
2023-03-01 18:01:47 +0700

    first commit

---
+++ ft_strrchr.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/01 14:15:29 by nsuphasa          #+#    #+#             */
+/*   Updated: 2023/03/01 17:58:03 by nsuphasa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strrchr(const char *s, int c)
+{
+	char	*ret;
+
+	ret = 0;
+	while (*s)
+	{
+		if (*s == (char)c)
+			ret = (char *)s;
+		s++;
+	}
+	if (*s == (char)c)
+		ret = (char *)s;
+	return (ret);
+}
+/*
+*************************************************************************
+DESCRIPTION
+locates last match character in string (including NULL)
+*************************************************************************
+RETURN
+located pointer // NULL (character not found)
+MANUAL
+https://clc-wiki.net/wiki/strrchr
+*************************************************************************
+ */
