58f36eacca756ee89eaf6c36a2f84b6db03d731a
522 <nakarinsupasatian@gmail.com>
2023-03-02 05:53:49 +0700

    add strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/21 15:26:28 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/02 03:32:04 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/02 04:29:36 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,4 +41,5 @@ char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *s, const char *find, size_t slen);
 #endif
