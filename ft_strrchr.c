06d7a900fff8370bde8f8979e73203de45a9dc55
522 <nakarinsupasatian@gmail.com>
2023-03-02 02:24:02 +0700

    add variation

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,11 +6,34 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 14:15:29 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/01 20:25:37 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/02 02:04:47 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strrchr(const char *s, int c)
+{
+	size_t	len;
+
+	len = ft_strlen(s) + 1;
+	while (len--)
+		if (s[len] == (char)c)
+			return ((char *)&s[len]);
+	return (NULL);
+}
+/*
+*************************************************************************
+DESCRIPTION
+locates last match character in string (including NULL)
+*************************************************************************
+RETURN
+located pointer || NULL (character not found)
+*************************************************************************
+MANUAL
+https://clc-wiki.net/wiki/strrchr
+*************************************************************************
+VERSION 1.0
 {
 	char	*ret;
 
@@ -25,15 +48,31 @@ char	*ft_strrchr(const char *s, int c)
 		ret = (char *)s;
 	return (ret);
 }
-/*
 *************************************************************************
-DESCRIPTION
-locates last match character in string (including NULL)
+VERSION 2.0
+{
+	char	*ret;
+
+	ret = 0;
+	s--;
+	while (*++s)
+		if (*s == (char)c)
+			ret = (char *)s;
+	if (*s == (char)c)
+		ret = (char *)s;
+	return (ret);
+}
 *************************************************************************
-RETURN
-located pointer || NULL (character not found)
-*************************************************************************
-MANUAL
-https://clc-wiki.net/wiki/strrchr
-*************************************************************************
+VERSION 3.0
+{
+	size_t	len;
+
+	len = ft_strlen(s);
+	if (s[len] == (char)c)
+		return ((char *)&s[len]);
+	while (len--)
+		if (s[len] == (char)c)
+			return ((char *)&s[len]);
+	return (NULL);
+}
  */
