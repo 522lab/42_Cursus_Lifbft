43a8e1cacec9baa511a6080d5a06f2a8603dd541
522 <nakarinsupasatian@gmail.com>
2023-03-02 00:52:30 +0700

    add variation

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 18:07:22 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/01 23:08:10 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/02 00:51:45 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,8 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	while ((*s1 || *s2) && n--)
-	{
-		if (*s1 != *s2)
-			return ((unsigned char)*s1 - (unsigned char)*s2);
-		s1++;
-		s2++;
-	}
+		if (*s1++ != *s2++)
+			return ((unsigned char)*--s1 - (unsigned char)*--s2);
 	return (0);
 }
 /*
@@ -41,18 +37,32 @@ if not check NUL, below result would be -1 which is incorrect.
 	strncmp("abcd\0e", "abcd\0f", 6)); // 0
 
 *************************************************************************
-2ND VERSION (USE INDEX)
+MANUAL
+https://clc-wiki.net/wiki/strncmp
+*************************************************************************
+VERSION 1.0
+{
 	unsigned int	i;
 
 	i = 0;
-	while (i < n && (s1[i] || s2[i]))
+	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
+}
 *************************************************************************
-MANUAL
-https://clc-wiki.net/wiki/strncmp
+VERSION 2.0
+{
+	while ((*s1 || *s2) && n--)
+	{
+		if (*s1 != *s2)
+			return ((unsigned char)*s1 - (unsigned char)*s2);
+		s1++;
+		s2++;
+	}
+	return (0);
+}
  */
