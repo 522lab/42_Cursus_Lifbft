651c4a716b09a836da03c7c307e8c52fb27672b1
522 <nakarinsupasatian@gmail.com>
2023-03-03 02:19:58 +0700

    1st commit

---
+++ ft_substr.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/03 00:59:37 by nsuphasa          #+#    #+#             */
+/*   Updated: 2023/03/03 02:11:44 by nsuphasa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+
+	if (!s)
+		return (NULL);
+	if (start > ft_strlen(s))
+		return (ft_strdup(""));
+	if (len > ft_strlen(&s[start]))
+		len = ft_strlen(&s[start]);
+	sub = malloc(len + 1);
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, &s[start], len + 1);
+	return (sub);
+}
+/*
+*************************************************************************
+DESCRIPTION
+allocated substring of size 'len' from start index of 's'
+*************************************************************************
+RETURN
+NULL (no src, allocate fail) || "" (exceed string) || pointer to sub (success)
+ */
