494997c1666ec5e7a503093ac60223e2201d455e
522 <nakarinsupasatian@gmail.com>
2023-03-02 22:07:43 +0700

    add links

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 21:46:23 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/02 22:01:00 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/02 22:06:35 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,12 @@ copy string to allocated memory
 *************************************************************************
 RETURN
 pointer to new string || NULL (allocate fail)
+*************************************************************************
+IMPLEMENTATION
+https://gist.github.com/reagent/3758387
+*************************************************************************
+MANUAL
+https://man7.org/linux/man-pages/man3/strdup.3.html
 *************************************************************************
 VERSION 1.0
 char	*ft_strdup(const char *s)
