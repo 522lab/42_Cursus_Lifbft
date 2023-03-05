dd05da70ee0ba731fa94b7c34372855ba3824329
522 <nakarinsupasatian@gmail.com>
2023-03-05 14:22:29 +0700

    add INT_MAX condition

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,12 +6,13 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/04 13:06:45 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/04 20:04:19 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/05 14:14:00 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
+#include <limits.h>
+#include "libft.h"
 
 static int	len_nbr(int nbr)
 {
@@ -33,17 +34,20 @@ char	*ft_itoa(int nbr)
 	int		len;
 	char	*str;
 
+	if (nbr == 0)
+		return (ft_strdup("0"));
+	if (nbr == -2147483648)
+		return (ft_strdup("-2147483648"));
 	len = len_nbr(nbr);
 	str = malloc(len + 1);
 	if (!str)
 		return (NULL);
-	str[len] = '\0';
 	if (nbr < 0)
 	{
 		nbr = -nbr;
 		str[0] = '-';
 	}
-	len--;
+	str[len--] = '\0';
 	while (nbr)
 	{
 		str[len] = (nbr % 10) + '0';
