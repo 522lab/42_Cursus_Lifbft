4d74584c231be623f863636fa6cefe480d6444f9
522 <nakarinsupasatian@gmail.com>
2023-03-26 16:50:02 +0700

    fix floating exception error

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nsuphasa <nsuphasa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 19:25:00 by nsuphasa          #+#    #+#             */
-/*   Updated: 2023/03/05 17:45:48 by nsuphasa         ###   ########.fr       */
+/*   Updated: 2023/03/26 16:49:40 by nsuphasa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*ptr;
 
-	if (nmemb >= SIZE_MAX / size || size >= SIZE_MAX / nmemb)
+	if (nmemb >= SIZE_MAX || size >= SIZE_MAX)
 		return (NULL);
 	ptr = malloc(nmemb * size);
 	if (ptr)
@@ -33,22 +33,6 @@ RETURN
 error (overflow) || pointer (success) || NULL (size, member = 0)
 -Moulinette
 NULL (overflow) || pointer (success) || NULL (size, member = 0)
-*************************************************************************
-WHY CANT ALLOCATE EXACTLY SIZE_MAX ?
-
-if so this will be enough,
-
-	if (nmemb > SIZE_MAX / size || size > SIZE_MAX / nmemb)
-
-but, maximum allocated memory using calloc depends on the system you use
-it is not guaranteed that you can allocate memory exactly SIZE_MAX long
-*************************************************************************
-WHY EQUAL CHECK IS NOT ENOUGH
-
-	if (nmemb == SIZE_MAX || size == SIZE_MAX)
-
-because checking equality of SIZE_MAX is not effectively preventing overflow
-since the product of (nmemb * size) may still exceed SIZE_MAX
 *************************************************************************
 MANUAL
 https://man7.org/linux/man-pages/man3/malloc.3.html
